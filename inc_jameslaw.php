c815414d83dd1d1e1e4ffb8da7c7eb6dd449d84d
azeem42 <azeemkhaan12343@gmail.com>
2023-05-13 23:04:43 +0500

    last assignement

--- inc_jameslaw.php
+++ inc_jameslaw.php
@@ -9,7 +9,7 @@
                 <b class="text_design">Leaderboard</b>
                 <div class="row mt-3">
                     <div class="col-xs-2 col-sm-2 col-lg-2">
-                        <img src="img/a2.jpg" class="rounded-circle" alt="Cinque Terre" height="35px" width="40px">
+                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="35px" width="40px">
                     </div>
                     <div class="col-xs-8 col-sm-8 col-lg-8">
                         <div class="ibox-content-adv" style="padding-left:8px;">
@@ -29,7 +29,47 @@
 
                 <div class="row mt-2">
                     <div class="col-xs-2 col-sm-2 col-lg-2">
-                        <img src="img/a2.jpg" class="rounded-circle" alt="Cinque Terre" height="35px" width="40px">
+                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="35px" width="40px">
+                    </div>
+                    <div class="col-xs-8 col-sm-8 col-lg-8">
+                        <div class="ibox-content-adv" style="padding-left:8px;">
+                            <div>
+                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" style="width: 66%" role="progressbar">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 col-sm-2 col-lg-2" style="margin-top: 12px;padding-left:0">
+                    <b>62</b> <i class="fa fa-home"></i>
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="col-xs-2 col-sm-2 col-lg-2">
+                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="35px" width="40px">
+                    </div>
+                    <div class="col-xs-8 col-sm-8 col-lg-8">
+                        <div class="ibox-content-adv" style="padding-left:8px;">
+                            <div>
+                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" style="width: 66%" role="progressbar">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 col-sm-2 col-lg-2" style="margin-top: 12px;padding-left:0">
+                    <b>62</b> <i class="fa fa-home"></i>
+                    </div>
+                </div>
+
+                <div class="row mt-2">
+                    <div class="col-xs-2 col-sm-2 col-lg-2">
+                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="35px" width="40px">
                     </div>
                     <div class="col-xs-8 col-sm-8 col-lg-8">
                         <div class="ibox-content-adv" style="padding-left:8px;">
---
+++ james3.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+
+
+<!-- Mirrored from webapplayers.com/inspinia_admin-v2.9.2/tabs.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 13 Sep 2019 10:06:06 GMT -->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Tabs </title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+  <link href="css/plugins/select2/select2.min.css" rel="stylesheet">
+  <!-- <script src="js/plugins/daterangepicker/daterangepicker.js"></script> -->
+  <link href="css/plugins/datapicker/datepicker3.css" rel="stylesheet">
+  <link href="css/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet">
+  
+<style>
+
+.text_design {
+            color: #203466;
+            font-size: 14px;
+        }
+.progress{
+            height: 7px;
+            width: 136px;
+}
+.bar_text{
+            font-size: 12px;
+}
+.progress-bar-success{
+    background-color: #203466;
+}
+.progress{
+            height: 6px;
+            width: 130px;
+}
+
+</style>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <nav class="navbar-default navbar-static-side" role="navigation">
+
+            <?php include('inc_leftmenu.php');  ?> 
+
+        </nav>
+
+        <div id="page-wrapper" class="gray-bg">
+            
+            <?php include('inc_test.php');  ?>
+
+            
+
+            <div class="footer">
+                <div class="float-right">
+                    10GB of <strong>250GB</strong> Free.
+                </div>
+                <div>
+                    <strong>Copyright</strong> Example Company &copy; 2014-2018
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <script src="js/plugins/pace/pace.min.js"></script>
+
+
+</body>
+
+
+<!-- Mirrored from webapplayers.com/inspinia_admin-v2.9.2/tabs.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 13 Sep 2019 10:06:06 GMT -->
+
+</html>
