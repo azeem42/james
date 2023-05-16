c048438a3d519bd7fea8406da8f562bd4f3e6735
azeem42 <azeemkhaan@gmail.com>
2023-05-16 13:29:35 +0500

    new commit

--- inc_test.php
+++ inc_test.php
@@ -176,14 +176,17 @@
            <div class="row">
         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" style="border:1px solid black;">
         <div class="ibox">
-                        <div class="ibox-content" style="border-radius:8px;">
-                                <h1 class="text-center"><b>56</b></h1>
-                                <span class="text-center"><b>Active Listing</b></span>    
-                        </div>
-                    </div>
+            <div class="ibox-content" style="border-radius:8px;">
+            <h1 class="text-center"><b>56</b></h1>
+            <span class="text-center"><b>Active Listing</b></span>    
+            </div>
+            </div>
            </div>
            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" style="border:1px solid black;">
-           
+           <div style="background-color: #ffffff;height:110px;border-radius:8px;">
+           <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">56</b></h1>
+           <h5 class="text-center"><b style="color:#203466;">Appraisals</b></h5>   
+        </div>
            </div>
            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" style="border:1px solid black;">
             
@@ -196,172 +199,11 @@
         </div>
         <div class="col-lg-3 mt-2">
             <div class="ibox-content" style="border-radius: 8px;">
-                <b class="text_design">Leaderboard</b>
+                
                 <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
+            
                 </div>
-
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-            </div>
-            <div class="ibox-content mt-3" style="border-radius: 8px;">
-                <b class="text_design">Leaderboard</b>
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2">
-                        <img src="img/profile_small.jpg" class="rounded-circle" alt="Cinque Terre" height="30px" width="35px">
-                    </div>
-                    <div class="col-xs-8 col-sm-8 col-lg-8">
-                        <div class="ibox-content-adv" style="padding-left:8px;">
-                            <div>
-                                <span class="bar_text"><b>Lonny Tian</b>&nbsp;&nbsp;&nbsp;&nbsp;<span><i class="fa fa-check text-warning"></i></span></span>
-                            </div>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success" style="width: 80px;" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-2 col-sm-2 col-lg-2 col-md-2" style="margin-top: 12px;padding-left:0">
-                        <b>62</b><i class="fa fa-home"></i>
-                    </div>
-                </div>
-
-            </div>
+            </div>       
         </div>
-    </div>
-   
+    </div> 
 </div>
