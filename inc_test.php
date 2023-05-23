03b767afa1fff7bd86b7b6ceff74f4e96a4648ce
azeem42 <azeemkhaan@gmail.com>
2023-05-23 14:40:21 +0500

    new commit

--- inc_test.php
+++ inc_test.php
@@ -109,111 +109,140 @@
 
             </div>
 
+
             <div class="ibox mt-3">
-            <div class="ibox-title" style="border-radius: 10px 10px 0px 0px;">
-            <b class="text_design">Open Home Reminder</b>
-            </div>
-            <div class="ibox-content" style="border-radius: 0px 0px 10px 10px;">
-           <div class="col-lg-12 text-center">
-            <i class="fa fa-calendar text_design"></i><b class="text_design">&nbsp;&nbsp;&nbsp;&nbsp;September, 2022</b>
-           </div>
-           <div class="row mt-2">
-            <div class="col-lg-1 center-text mr-2 days_cal">MON</div>
-            <div class="col-lg-1 center-text mr-2 days_cal">TUE</div>
-            <div class="col-lg-1 center-text mr-2 days_cal">WED</div>
-            <div class="col-lg-1 center-text mr-2 days_cal">THU</div>
-            <div class="col-lg-1 center-text mr-2 days_cal">FRI</div>
-            <div class="col-lg-1 center-text mr-2 days_cal">SAT</div>
-            <div class="col-lg-1 center-text mr-2 days_cal">SUN</div>
-           </div>
-           <div class="row mt-2">
-            <div class="col-lg-1 center-text ml-2 date_cal"><b>8</b></div>
-            <div class="col-lg-1 center-text mr-2 date_cal"><b>9</b></div>
-            <div class="col-lg-1 center-text mr-2 date_cal"><b>10</b></div>
-            <div class="col-lg-1 center-text mr-2 date_cal"><b>11</b></div>
-            <div class="col-lg-1 center-text mr-2 date_cal"><b>12</b></div>
-            <div class="col-lg-1 center-text mr-2 date_cal"><b>13</b></div>
-            <div class="col-lg-1 center-text mr-2 date_cal"><b>14</b></div>
-           </div>
-           <div class="row">
-            <div class="col-lg-12 mt-3" style="border:1px solid green;">
+                <div class="ibox-title" style="border-radius: 10px 10px 0px 0px;">
+                    <b class="text_design">Open Home Reminder</b>
+                </div>
+                <div class="ibox-content" style="border-radius: 0px 0px 10px 10px;">
+                    <div class="col-lg-12 text-center">
+                        <i class="fa fa-calendar text_design"></i><b class="text_design">&nbsp;&nbsp;&nbsp;&nbsp;September, 2022</b>
+                    </div>
 
-            <div class="slick_demo_1">
 
-<div>
-    <div class="ibox-content" style="background-color: lightgrey;">
-        <h4 class="text_design"><b>jl23454</b></h4>
-        <p>
-            56 / 241 Flat Bush School Road,<br>Flat Bush, Manukau City...
-        </p>
-        <h4 class="text_design"><b>12:00PM - 8:00PM</b></h4>
-        <h4 class="text_design"><b>Joyce, Corbin</b></h4>
-    </div>
-</div>
-<div>
-<div class="ibox-content" style="background-color: lightgrey;">
-        <h4 class="text_design"><b>jl23454</b></h4>
-        <p>
-            56 / 241 Flat Bush School Road,<br>Flat Bush, Manukau City...
-        </p>
-        <h4 class="text_design"><b>12:00PM - 8:00PM</b></h4>
-        <h4 class="text_design"><b>Joyce, Corbin</b></h4>
-    </div>
-</div>
-<div>
-<div class="ibox-content" style="background-color: lightgrey;">
-        <h4 class="text_design"><b>jl23454</b></h4>
-        <p>
-            56 / 241 Flat Bush School Road,<br>Flat Bush, Manukau City...
-        </p>
-        <h4 class="text_design"><b>12:00PM - 8:00PM</b></h4>
-        <h4 class="text_design"><b>Joyce, Corbin</b></h4>
-    </div>
-</div>
-</div>
+                    <div class="row">
+                        <div class="col-lg-12 mt-3">
 
+                            <div class="slick_demo_1">
+
+                                <div>
+                                    <div class="ibox-content" style="background-color: lightgrey;border-radius:8px;">
+                                        <h4 class="text_design"><b>jl23454</b></h4>
+                                        <p>
+                                            56 / 241 Flat Bush School Road,<br>Flat Bush, Manukau City...
+                                        </p>
+                                        <h4 class="text_design"><b>12:00PM - 8:00PM</b></h4>
+                                        <h4 class="text_design"><b>Joyce, Corbin</b></h4>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="ibox-content" style="background-color: lightgrey;">
+                                        <h4 class="text_design"><b>jl23454</b></h4>
+                                        <p>
+                                            56 / 241 Flat Bush School Road,<br>Flat Bush, Manukau City...
+                                        </p>
+                                        <h4 class="text_design"><b>12:00PM - 8:00PM</b></h4>
+                                        <h4 class="text_design"><b>Joyce, Corbin</b></h4>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="ibox-content" style="background-color: lightgrey;">
+                                        <h4 class="text_design"><b>jl23454</b></h4>
+                                        <p>
+                                            56 / 241 Flat Bush School Road,<br>Flat Bush, Manukau City...
+                                        </p>
+                                        <h4 class="text_design"><b>12:00PM - 8:00PM</b></h4>
+                                        <h4 class="text_design"><b>Joyce, Corbin</b></h4>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-           </div>
-            </div>
-            </div>
-        
+
+
+
+
+
+
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mt-2" style="height:113px;">
-           <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-         <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
-           <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">56</b></h1>
-           <h5 class="text-center"><b style="color:#203466;">Active Listing</b></h5>   
-        </div>
-           </div>
-           <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-           <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
-           <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">29</b></h1>
-           <h5 class="text-center"><b style="color:#203466;">Appraisals</b></h5>   
-        </div>
-           </div>
-           <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-             <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
-           <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">14</b></h1>
-           <h5 class="text-center"><b style="color:#203466;">Under Contract</b></h5>   
-        </div>
-           </div>
-           <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-             <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
-           <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">33</b></h1>
-           <h5 class="text-center"><b style="color:#203466;">Unconditionals</b></h5>   
-        </div>
-           </div> 
-           </div>
-          
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
+                        <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">56</b></h1>
+                        <h5 class="text-center"><b style="color:#203466;">Active Listing</b></h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
+                        <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">29</b></h1>
+                        <h5 class="text-center"><b style="color:#203466;">Appraisals</b></h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
+                        <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">14</b></h1>
+                        <h5 class="text-center"><b style="color:#203466;">Under Contract</b></h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <div style="background-color: #ffffff;height:110px;border-radius:8px;border-bottom: 3px solid orange;">
+                        <h1 class="text-center" style="margin-top:0px;"><b style="font-size: 53px;color: #203466;font-weight:bold;">33</b></h1>
+                        <h5 class="text-center"><b style="color:#203466;">Unconditionals</b></h5>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <div class="col-lg-3 mt-2">
             <div class="ibox-content" style="border-radius: 8px;">
-                
                 <div class="row mt-3">
-            
+
                 </div>
-            </div>       
+            </div>
+
+            <div class="ibox mt-3" style="border-radius: 8px;">
+                <div class="ibox-title" style="border-radius: 10px 10px 0px 0px;">
+                    <b class="text_design">Upcoming Expiry Listings</b>
+                </div>
+
+                <div class="ibox-content" style="border-radius:8px;">
+
+                   
+
+                    <div class="row">
+                        <div class="col-lg-12 mt-3">
+
+                            <div class="slick_demo_1">
+
+                                <!-- <div> -->
+                                    <div class="ibox-content" style="border-radius:8px;">
+                                        
+                                       <img src="" class="img/profile_small.jpg" alt="">
+                                    </div>
+                                <!-- </div> -->
+                                <!-- <div> -->
+                                <div class="ibox-content" style="border-radius:8px;">
+                                        
+                                       
+                                        </div>
+                                <!-- </div> -->
+                                <!-- <div> -->
+                                <div class="ibox-content" style="border-radius:8px;">
+                                        
+                                       
+                                        </div>
+                                <!-- </div> -->
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
-    </div> 
-</div>
+    </div>
